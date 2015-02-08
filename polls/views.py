6ab762fe60115511fea4d9cf479f733b496187cd
kbrandenburg <keith@keithbrandenburg.com>
2015-02-08 11:24:10 -0600

    version of views before rewrite to use generic views

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse 
+#from django.template import RequestContext, loader
+
+from polls.models import Question,Choice
 
 # Create your views here.
+old_way = """
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    #output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(template.render(context))"""
+
+# this idiom to replace the above index is pretty badass
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # always return response redirect after dealing with POST 
+        # to prevent reposts if user hits back button
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+
+
+
+
+def polio(request):
+    return HttpResponse("You got it.")
